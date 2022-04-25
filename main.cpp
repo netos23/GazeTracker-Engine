1a1a527588c4a4f232405b22a3fca71bd6d5c1df
netos23 <nikmoroz2002@outlook.com>
2022-04-25 23:20:03 +0300

    add face detection

--- main.cpp
+++ main.cpp
@@ -2,49 +2,45 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
+#include "dlib/image_processing/frontal_face_detector.h"
+#include "dlib/opencv/cv_image.h"
+#include <dlib/image_processing.h>
 #include <iostream>
 
-using namespace std;
 using namespace cv;
+using namespace dlib;
 
 void detectAndDisplay(Mat frame);
 
-CascadeClassifier face_cascade;
-CascadeClassifier eyes_cascade;
+frontal_face_detector hogFaceDetector;
 
 int main(int argc, const char **argv) {
+    float temp = 0;
+    if (temp != 0 && fpclassify(temp) != FP_SUBNORMAL) {}
+    else {
+
+    }
     CommandLineParser parser(argc, argv,
                              "{help h||}"
-                             "{face_cascade|opencv/data/haarcascades/haarcascade_frontalface_alt.xml|Path to face cascade.}"
-                             "{eyes_cascade|opencv/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml|Path to eyes cascade.}"
                              "{camera|0|Camera device number.}");
     parser.about(
             "\nThis program demonstrates using the cv::CascadeClassifier class to detect objects (Face + eyes) in a video stream.\n"
             "You can use Haar or LBP features.\n\n");
     parser.printMessage();
-    String face_cascade_name = samples::findFile(parser.get<String>("face_cascade"));
-    String eyes_cascade_name = samples::findFile(parser.get<String>("eyes_cascade"));
-    //-- 1. Load the cascades
-    if (!face_cascade.load(face_cascade_name)) {
-        cout << "--(!)Error loading face cascade\n";
-        return -1;
-    };
-    if (!eyes_cascade.load(eyes_cascade_name)) {
-        cout << "--(!)Error loading eyes cascade\n";
-        return -1;
-    };
+    hogFaceDetector = get_frontal_face_detector();
+
     int camera_device = parser.get<int>("camera");
     VideoCapture capture;
     //-- 2. Read the video stream
     capture.open(camera_device);
     if (!capture.isOpened()) {
-        cout << "--(!)Error opening video capture\n";
+        std::cout << "--(!)Error opening video capture\n";
         return -1;
     }
     Mat frame;
     while (capture.read(frame)) {
         if (frame.empty()) {
-            cout << "--(!) No captured frame -- Break!\n";
+            std::cout << "--(!) No captured frame -- Break!\n";
             break;
         }
         //-- 3. Apply the classifier to the frame
@@ -53,6 +49,7 @@ int main(int argc, const char **argv) {
             break; // escape
         }
     }
+
     return 0;
 }
 
@@ -61,21 +58,22 @@ void detectAndDisplay(Mat frame) {
     cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
     equalizeHist(frame_gray, frame_gray);
     //-- Detect faces
-    std::vector<Rect> faces;
-    face_cascade.detectMultiScale(frame_gray, faces);
-    for (size_t i = 0; i < faces.size(); i++) {
-        Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
-        ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4);
-        Mat faceROI = frame_gray(faces[i]);
-        //-- In each face, detect eyes
-        std::vector<Rect> eyes;
-        eyes_cascade.detectMultiScale(faceROI, eyes);
-        for (size_t j = 0; j < eyes.size(); j++) {
-            Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
-            int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
-            circle(frame, eye_center, radius, Scalar(255, 0, 0), 4);
-        }
+    cv_image<bgr_pixel> dlibIm(frame);
+    std::vector<dlib::rectangle> faceRects = hogFaceDetector(dlibIm);
+
+    for (auto &faceRect: faceRects) {
+
+        long x1 = faceRect.left();
+        long y1 = faceRect.top();
+        long x2 = faceRect.right();
+        long y2 = faceRect.bottom();
+
+        cv::rectangle(frame, Point(x1, y1), Point(x2, y2),
+                      Scalar(0, 255, 0), 1, 4);
+
     }
+
+
     //-- Show what you got
     imshow("Capture - Face detection", frame);
 }
