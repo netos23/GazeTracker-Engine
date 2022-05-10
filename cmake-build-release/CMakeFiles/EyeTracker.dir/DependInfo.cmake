fe9a8a53ea4adabee17dfe9370be14a1118d8af5
netos23 <nikmoroz2002@outlook.com>
2022-05-11 01:09:29 +0300

    update-config

---
+++ cmake-build-release/CMakeFiles/EyeTracker.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/nikmo/CLionProjects/GazeTracker-Engine/main.cpp" "C:/Users/nikmo/CLionProjects/GazeTracker-Engine/cmake-build-release/CMakeFiles/EyeTracker.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/openCV/opencv/build/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release/CMakeFiles/EyeTracker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EyeTracker.dir/main.cpp.obj"
+  "EyeTracker.exe"
+  "EyeTracker.exe.manifest"
+  "EyeTracker.lib"
+  "EyeTracker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EyeTracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
